735bfce55584c57629ad547890f0bf6b3de1c1c3
bcooper085 <bcooper085@gmail.com>
2017-03-24 08:48:20 -0700

    add index template and route

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # questions-answers
 
-This README outlines the details of collaborating on this Ember application.
-A short introduction of this app could easily go here.
+Users can post, update and delete questions. Users can also post responses/answers to questions.
 
 ## Prerequisites
 
---
+++ app/routes/index.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+      return Ember.RSVP.hash({
+        questions: this.store.findAll('question'),
+        answers: this.store.findAll('answer')
+      });
+    },
+    actions: {
+        saveQuestion(params) {
+            var newQuestion = this.store.createRecord('question', params);
+            newQuestion.save();
+            this.transitionTo('index');
+        },
+
+        editQuestion(question, params) {
+            Object.keys(params).forEach(function(key) {
+                if(params[key]!==undefined) {
+                    question.set(key,params[key]);
+                }
+            });
+            question.save();
+            this.transitionTo('index');
+        },
+
+        destroyQuestion(question) {
+            question.destroyRecord();
+            this.transitionTo('index');
+          },
+    }
+});
