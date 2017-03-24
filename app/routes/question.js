d5311e49c93446ba4f093c598a444e2ede895833
bcooper085 <bcooper085@gmail.com>
2017-03-24 12:17:06 -0700

    add new answer form and js save function

--- app/routes/question.js
+++ app/routes/question.js
@@ -6,18 +6,28 @@ export default Ember.Route.extend({
   },
   actions: {
     editQuestion(question, params) {
-        Object.keys(params).forEach(function(key) {
-            if(params[key]!==undefined) {
-                question.set(key,params[key]);
-            }
-        });
-        question.save();
-        this.transitionTo('index');
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
     },
 
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
-    }
+    },
+
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    },
   }
 });
